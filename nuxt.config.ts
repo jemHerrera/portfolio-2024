dce505b3f7060b22e1d6d0683540a44bc53033c3
Jem Herrera <jhneramuz@gmail.com>
2024-10-14 16:08:40 -0600

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/main.css"],
+  router: {
+    base: "/portfolio-2024/",
+  },
+  target: "static",
 });
