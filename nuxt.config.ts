249838f88fc7413f1e851eb8a67a90d9bd94a329
Jem Herrera <jhneramuz@gmail.com>
2024-10-14 15:04:06 -0600

    Github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/main.css"],
+  app: {
+    baseURL: "/portfolio-2024/",
+  },
 });
